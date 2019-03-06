a1fe8847e6fd32fafc526a037652f03c847281e4
Shubham Naik <shubham.naik10@gmail.com>
2019-03-05 23:37:44 -0800

    add distribute script

--- cli/setup.py
+++ cli/setup.py
@@ -6,7 +6,7 @@ def read_file(fname):
 
 setup(
     name="quicklinks",
-    version='0.1.0',
+    version='0.1.1',
     author='Shubham Naik',
     author_email='shub@shub.club',
     description='Quickly navigate to websites based on shorthands you provide',

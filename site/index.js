db80430ea07ccc467b43e40d0402d505b5deed26
Shubham Naik <shubham.naik10@gmail.com>
2019-03-15 23:26:31 -0700

    feature | add ui components

---
+++ site/index.js
@@ -0,0 +1,34 @@
+/**
+ * Renders a nice play button and ui around it
+ */
+function renderPlayButton() {
+    const playContainer = document.getElementById('demo-play-container');
+    const playButton = document.getElementById('demo-play-button');
+    const demoVideo = document.getElementById('demo-video');
+
+    demoVideo.controls = false;
+
+    playContainer.style.display = 'flex';
+
+    /**
+     * Plays video, returns video control tools back
+     */
+    function playVideo() {
+        demoVideo.play();
+
+        playContainer.style.display = 'none';
+        demoVideo.controls = true;
+    }
+
+    playContainer.addEventListener('click', playVideo);
+
+    playButton.addEventListener('keypress', function (e) {
+        if (e.keyCode == 13) {
+            playVideo();
+        }
+    });
+}
+
+window.onload = function () {
+    renderPlayButton();
+};
